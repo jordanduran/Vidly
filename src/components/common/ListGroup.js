ebd0a223c523c8aae879d54677e3d17228b0998e
Jordan Duran <jordanduran@Jordans-MacBook-Pro.local>
2020-04-22 15:17:13 -0400

    Class typo

--- src/components/common/ListGroup.js
+++ src/components/common/ListGroup.js
@@ -6,7 +6,7 @@ const ListGroup = props => {
   return (
     <ul className='list-group'>
       {items.map(item => (
-        <li key={item[valueProperty]} className='list -group-item'>
+        <li key={item[valueProperty]} className='list-group-item'>
           {item[textProperty]}
         </li>
       ))}
